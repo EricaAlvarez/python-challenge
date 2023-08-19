dc0c8c53f041568c5cd8d68eeedee9cc368441c5
Erica Álvarez <alvarezhernandez.erica@gmail.com>
2023-08-19 17:22:16 -0600

    Try 2

--- PyBank/main.py
+++ PyBank/main.py
@@ -19,27 +19,47 @@ with open(budget_path) as csv_file:
     # Read the header row first
     header = next(csv_reader)
 
-    # Calculates the number of months
-
+    # List creation (to hold the values from the columns later)
     Date = []
     PnL = []
-    
+    Change = []
+    Monthly_change = []
+
+    # Starting values
+    greatest_increase = 0
+    increase_month = ''
+    greatest_decrease = 0
+    decrease_month = ''
+
+    # Foor loop trough the rows
     for row in csv_reader:
 
+        # Adds every new value into a list
         Date.append(row[0])
-        PnL.append(row[1])
-        
-        print (PnL)
-        
-        total = sum(int(PnL))
+        PnL.append(int(row[1]))
+        Change.append(int(row[1]))
+             
+        # Determinates de months lists lengh
+        months = len(Date)
 
+        # Finds the biggest increase and decrease
+        if int(row[1]) > greatest_increase:
+            increase_month = (row[0])
+            greatest_increase = int(row[1])
+        elif int(row[1]) < greatest_decrease:
+            decrease_month = (row[0])
+            greatest_decrease = int(row[1])
 
+    # Calculates proffit and losses total
+    total = sum(PnL,3)
+    
+    # Follows monthly changes
+    for i in range(len(Change)-1):
+        change_per_month = (Change[i+1] - Change[i])
+        Monthly_change.append(change_per_month)
 
-
-
-
-
-
+    # Calculate average
+    average_change = round(sum(Monthly_change)/len(Monthly_change),2)
 
 # PRINTS THE FOLLOWING VALUES:
 '''
@@ -49,3 +69,25 @@ with open(budget_path) as csv_file:
     The greatest increase in profits (date and amount) over the entire period
     The greatest decrease in profits (date and amount) over the entire period
 '''
+
+print(f"""
+      Financial Analysis
+      ----------------------------
+      Total Months: {months}
+      Total: $ {total}
+      Averae Change: $ {average_change}
+      Greatest Increase in Profits: {increase_month} $ {greatest_increase}
+      Greatest Decrease in Profits: {decrease_month} $ {greatest_decrease}
+      """)
+
+# EXPORTS RESULTS TO TEXT FILE:
+
+f = open("financial_analysis.txt", "w")
+f.write("Financial Analysis")
+f.write("___________________________________")
+
+f.write("Total Months: " + str(months))
+f.write("Average Change is: $" + str(round(average_change, 2)))
+f.write("Total: $" + str(total))
+f.write("Greatest Increase in Profits: " + str(increase_month) + "  ($" + str(greatest_increase) + ")")
+f.write("Greatest Decrease in Profits: " + str(decrease_month) + "  ($" + str(greatest_decrease) + ")")
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,22 +1,79 @@
 # # # PyPoll # # #
 
 
-# CALCULATES THE TOTAL NUMBER OF VOTES
+# Import the os module (allows to create file paths across operating system)
+import os
+
+# Imports CVS module
+import csv
+
+# Imports currency and date formatting
+import locale
+
+# Follows the file path
+election_path = os.path.join("PyPoll","Resourses","election_data.csv")
 
 
+# Dictionary creation (to hold the cadidates names) + starting values
+Candidate_votes = {}
+Votes = 0
 
-# RETURNS A COMPLETE LIST OF CANDIDATES WHO RECEIVED VOTES
+# Open and read csv
+with open(election_path, newline='') as csv_file:
+    csv_reader = csv.reader(csv_file)
+
+    # Read the header row first
+    header = next(csv_reader)
+
+    # Foor loop trough the rows
+    for row in csv_reader:
+        Votes += 1
+        if row[2] not in Candidate_votes :
+            Candidate_votes [row[2]] = 1
+        else:
+            Candidate_votes [row[2]] += 1  
+
+winner = max(Candidate_votes, key=Candidate_votes.get)
 
 
+# PRINTS THE FOLLOWING VALUES TO TERMINAL:
+'''
+    The total number of votes cast
+    A complete list of candidates who received votes
+    The percentage of votes each candidate won
+    The total number of votes each candidate won
+    The winner of the election based on popular vote
+'''
 
-# CALCULATES THE PERCENTAGE OF VOTES EACH CANDIDATE WON
+print(f"""Election Result
+----------------------------
+Total Votes: {Votes}
+-------------------------""")
+for candidate, votes in Candidate_votes.items():
+        print(candidate + ": " + "{:.3%}".format(votes/Votes) + "   (" +  str(votes) + ")")
 
+print(f"""----------------------------
+Winner: {winner}
+----------------------------
+""")
 
+# EXPORTS RESULTS TO TEXT FILE:
 
-# CALCULATES THE TOTAL NUMBER OF VOTES EACH CANDIDATE WON
+f = open("election_results.txt", "w")
+f.write("Election Results")
+f.write('\n')
+f.write("-------------------------")
+f.write('\n')
+f.write("Total Votes: " + str(Votes))
+f.write('\n')
+f.write("-------------------------")
+f.write('\n')
 
-
-
-# THE WINNER OF THE ELECTION BASED ON POPULAR VOTE
-
-
+for candidate, votes in Candidate_votes.items():
+    f.write(candidate + ": " + "{:.3%}".format(votes/Votes) + "   (" +  str(votes) + ")")
+    f.write('\n')
+  
+f.write("-------------------------") 
+f.write('\n')
+f.write(f"Winner: {winner}")
+f.write('\n')
